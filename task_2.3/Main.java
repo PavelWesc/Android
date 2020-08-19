5b8318adc6bdc6ff97578d49da03990d41e2e585
PavelWesc <69851144+PavelWesc@users.noreply.github.com>
2020-08-19 18:46:36 +0300

    Update Main.java

--- task_2.3/Main.java
+++ task_2.3/Main.java
@@ -12,7 +12,11 @@
 
 public class Main {
     public static void main(String[] args) {
-       System.out.println(1*2*3*4*5*6*7*8*9*10);
+       int sum = 1;
+        int i;
+        for (i=1; i<=10; i++)
+        sum = sum * i;{}
+        System.out.println(sum);
 
     }
 }
