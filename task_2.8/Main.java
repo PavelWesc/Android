a3df05a8e0d22424942fb360d13b5e176228a029
PavelWesc <69851144+PavelWesc@users.noreply.github.com>
2020-08-26 12:50:34 +0300

    Update Main.java

--- task_2.8/Main.java
+++ task_2.8/Main.java
@@ -14,13 +14,14 @@
 */
 
 public class Main {
-    public static void main(String[] args) {
-        int a;
-        int b;
-        for(a = 0 ; a < 10; a++){
-            System.out.println();
-            for(b=0 ; b <= a; b++)
-                System.out.print("8");
+   public static void main(String[] args) {
+            int a;
+            int b;
+            for (a = 0; a < 10; a++) {
+                System.out.println();
+                for (b = 0; b <= a; b++)
+                    System.out.print("8");
 
+            }
+        }
     }
-}
