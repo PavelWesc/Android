3b8c3ff13c5c2bc4be384420305d2e2acb6e65ba
PavelWesc <69851144+PavelWesc@users.noreply.github.com>
2020-08-26 14:50:01 +0300

    Update Main.java

--- task_2.7/Main.java
+++ task_2.7/Main.java
@@ -8,15 +8,15 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(sumDigitsInNumber(546));
-    }
+            System.out.println(sumDigitsInNumber(511));
+        }
 
-    public static int sumDigitsInNumber(int number) {
-        int a = 546/100;
-        int b = ((546 - a * 100)/10);
-        int c = ((546 - a * 100)-b*10);
-        return a+b+c;
+        public static int sumDigitsInNumber(int number) {
+            int a = (number)/100;
+            int b = (((number) - a * 100)/10);
+            int c = (((number) - a * 100)-b*10);
+            return a+b+c;
 
-        
+
+        }
     }
-}
